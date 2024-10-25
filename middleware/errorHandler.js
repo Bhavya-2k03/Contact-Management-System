05bccbb2b4e9cdb18cf6add12e684b3acffab860
Bhavya <bhavya.2k03@gmail.com>
2024-10-25 21:07:32 +0530

    Inital commit

---
+++ middleware/errorHandler.js
@@ -0,0 +1,5 @@
+const errorHandler=(error,req,res,next)=>{
+    const statuscode=res.statuscode ? res.statuscode :500;
+}
+
+module.exports=errorHandler;
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express=require('express');
+const errorHandler=require('./middleware/errorhandler');
 const dotenv=require('dotenv').config();
 
 const app=express();
@@ -6,6 +7,7 @@ const port=process.env.PORT  || 5000;
 
 app.use(express.json()); //provides a parser to parse incoming requests, otherwise console.log(req.body) will give undefined.
 app.use('/api/contacts',require('./routes/contactRoutes'));
+app.use(errorHandler);
 
 app.listen(port,()=>{  
     console.log(`Server is listning on port ${port}`);
