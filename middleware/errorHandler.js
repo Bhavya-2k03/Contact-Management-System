a896777f79080bc08d26cbf2e3e1dcba0bf9b96d
Bhavya <bhavya.2k03@gmail.com>
2024-10-25 22:54:08 +0530

    Initial commit

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,5 +1,16 @@
-const errorHandler=(error,req,res,next)=>{
+const errorHandler=(err,req,res,next)=>{
     const statuscode=res.statuscode ? res.statuscode :500;
+    
+    switch(statuscode) 
+    {
+        case 400:
+            res.json({title:"Validation error",message:err.message, stackTrace:err.stack});
+            break;
+        case 404:
+            res.json({title:"Not Found", message:err.message,stackTrace:err.stack});
+        default:
+            break;
+    }
 }
 
-module.exports=errorHandler;
+module.exports=errorHandler;     
